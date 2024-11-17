877d24e05cd7098b08011389261ed4a924ea6c3d
Viserion77 <jeferson.a.oficial@gmail.com>
2024-11-17 16:31:32 +0000

    feat(infra): :lock: add SSL configuration for PostgreSQL client based on environment

--- infra/database.ts
+++ infra/database.ts
@@ -8,6 +8,7 @@ async function createClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV !== "development"
   });
 
   await client.connect();
