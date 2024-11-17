f9d8a1bab1dd786efecbabb50e403fee8056f162
Viserion77 <jeferson.a.oficial@gmail.com>
2024-11-17 19:42:37 +0000

    feat(DX): :test_tube: add Vitest configuration for Nuxt testing environment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   pages: true,
+  modules: ["@nuxt/test-utils/module"],
+  ignore: ["**/*.test.ts"],
 });
