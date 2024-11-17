67d88363e0c2ebcfa76b5b125dd8744646fed57f
Jeferson Alves <jeferson.a.oficial@gmail.com>
2024-11-17 16:12:37 +0000

    feat(infra): :bricks: add PostgreSQL client and query functionality

---
+++ infra/database.ts
@@ -0,0 +1,24 @@
+import pg from "pg";
+const { Client } = pg;
+
+async function createClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT ?? 5432),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  await client.connect();
+  return client;
+}
+
+const clientPromise = createClient();
+
+async function query(queryObject: string | { text: string; values: any[] }) {
+  const client = await clientPromise;
+  return client.query(queryObject);
+}
+
+export default { query };
