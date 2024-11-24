87d1a4e8522cecd6d8a3870704c183d19550ad0a
Jeferson Alves <jeferson.a.oficial@gmail.com>
2024-11-24 21:46:00 +0000

    test(migrations): :white_check_mark: enhance POST migration tests with database cleanup and validation

--- server/api/v1/migrations/post.test.ts
+++ server/api/v1/migrations/post.test.ts
@@ -1,12 +1,33 @@
-import { test, expect } from "vitest";
+import { test, expect, beforeAll } from "vitest";
+import database from "~/resources/database";
+
+async function cleanDatabase(){
+database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+}
+
+beforeAll(cleanDatabase);
 
 test.sequential("POST /api/v1/migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  expect(response.status).toBe(200);
+  expect(response1.status).toBe(201);
 
-  const responseBody = await response.json();
+  const responseBody1 = await response1.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
+  expect(Array.isArray(responseBody1)).toBe(true);
+  expect(responseBody1.length).toBeGreaterThan(0);
+
+  const migrationsResponse = await database.query("SELECT count(*)::int FROM pgmigrations");
+  expect(migrationsResponse.rows[0].count).toBeGreaterThan(0);
+  
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const responseBody2 = await response2.json();
+
+  expect(Array.isArray(responseBody2)).toBe(true);
+  expect(responseBody2.length).toBe(0);
 });
