18daf4f5667e1f84bda5bcea258dc47957410d76
Jeferson Alves <jeferson.a.oficial@gmail.com>
2024-11-24 21:46:05 +0000

    test(migrations): :white_check_mark: add database cleanup before tests and validate response length

--- server/api/v1/migrations/get.test.ts
+++ server/api/v1/migrations/get.test.ts
@@ -1,4 +1,11 @@
-import { test, expect } from "vitest";
+import { test, expect, beforeAll } from "vitest";
+import database from "~/resources/database";
+
+async function cleanDatabase(){
+database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+}
+
+beforeAll(cleanDatabase);
 
 test.sequential("GET /api/v1/migrations", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
@@ -7,4 +14,5 @@ test.sequential("GET /api/v1/migrations", async () => {
   const responseBody = await response.json();
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
