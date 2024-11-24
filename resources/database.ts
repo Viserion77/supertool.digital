5d32b1f1b80073bf651789f95971a2046a21df75
Jeferson Alves <jeferson.a.oficial@gmail.com>
2024-11-24 21:45:10 +0000

    refactor(resources): :recycle: update SSL configuration for production environment

--- resources/database.ts
+++ resources/database.ts
@@ -8,7 +8,7 @@ async function createClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV !== "development",
+    ssl: process.env.NODE_ENV === "production",
   });
 
   await client.connect();
