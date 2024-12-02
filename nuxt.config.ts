f8564acc8ea532dea5ae9d36e015b757aa57a531
Jeferson Alves <jeferson.a.oficial@gmail.com>
2024-12-02 01:46:21 +0000

    chore(config): :truck: remove unused fs-extra dependency and related build hook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from "nuxt/config";
-import { resolve } from "path";
-import { copySync } from "fs-extra";
-
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   pages: true,
   modules: ["@nuxt/test-utils/module"],
   ignore: ["**/*.test.ts"],
-  hooks: {
-    "build:before": () => {
-      const src = resolve(__dirname, "resources/migrations");
-      const dest = resolve(__dirname, ".nuxt/resources/migrations");
-      copySync(src, dest);
-    },
-  },
 });
