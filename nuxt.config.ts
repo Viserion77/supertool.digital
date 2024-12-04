d419afe3f6ac8c42e0490ddc5751823d3d3fb7f5
Jeferson Alves <jeferson.a.oficial@gmail.com>
2024-12-04 00:02:21 +0000

    feat(config): :sparkles: add Vercel functions configuration for migrations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   pages: true,
   modules: ["@nuxt/test-utils/module"],
   ignore: ["**/*.test.ts"],
+  nitro: {
+    vercel: {
+      functions: {
+        migrations: {
+          handler: "~/server/api/v1/migrations/index.ts",
+          events: ["http"],
+        },
+      },
+    }
+  }
+
 });
