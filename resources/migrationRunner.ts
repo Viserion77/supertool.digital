c072f1570572cdbe3e635f71393dfdf111d0ac90
Viserion77 <jeferson.a.oficial@gmail.com>
2024-12-07 00:15:44 +0000

    feat(migration): :sparkles: enhance migration runner with detailed logging and improved query execution

--- resources/migrationRunner.ts
+++ resources/migrationRunner.ts
@@ -30,23 +30,28 @@ export async function migrationRunner({
   }
 
   const migrationFiles = Object.keys(migrations).sort();
+
+  console.info("> Migrating files:");
+
   const migrationList = [];
   for (const migration of migrationFiles) {
     if (migration <= lastMigration) {
       continue;
     }
+    console.info(`> - ${migration}`);
     migrationList.push(migration);
 
     const query = migrations[migration]();
-    const result = dryRun ? {} : await db.query(query);
-    console.info("migration result", migration, result);
+
+    console.info(`### MIGRATION ${migration} (UP) ###`);
+    if (!dryRun) await db.query(query);
 
     const runOn = new Date().toISOString();
-    await db.query(
-      `INSERT INTO ${migrationsTable} (name, run_on) VALUES ('${migration}', '${runOn}')`,
-    );
-    console.log(`Migração executada: ${migration}`);
+    const queryMigration = `INSERT INTO ${migrationsTable} (name, run_on) VALUES ('${migration}', '${runOn}')`;
+    console.info(queryMigration);
+    await db.query(queryMigration);
   }
+  console.log(`\n\nMigrations complete!`);
 
   if (closeConnection) {
     await (await db.client).end();
